929c35973aeec7f694129fd79be045b9b7e01bc7
8778saquib <saquib.nawaz009@gmail.com>
2022-05-07 01:36:17 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linecompprob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison problem");
+        }
+    }
+}
